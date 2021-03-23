55097d1cca82abbefe695d08eb1f29af3ddec5d5
karelVR <karel.vanroey@cegeka.com>
2021-03-23 16:46:31 +0100

    Hey! Second commit => multimodule setup!

---
+++ war/src/main/java/com/switchfully/digibooky/DigibookyApp.java
@@ -0,0 +1,11 @@
+package com.switchfully.digibooky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigibookyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DigibookyApp.class);
+    }
+}
---
+++ war/src/test/java/com/switchfully/digibooky/DigibookyAppTest.java
@@ -0,0 +1,14 @@
+package com.switchfully.digibooky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DigibookyAppTest {
+    @Test
+    void contextTest() {
+        // Test the spring context
+    }
+}
